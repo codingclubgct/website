06052f14bd2695b0e15b669eea2c3803fafcc064
iamnoufal <iamnoufal@users.noreply.github.com>
2024-01-28 04:12:42 +0000

    deploy: coding-club-gct/homepage@16111f4473297adb3b8ca8a076577805883c59ff

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -37,13 +37,13 @@ const sidebarItems = [
 export default function Sidebar({ open, setOpen }: { open: boolean, setOpen: Dispatch<SetStateAction<boolean>> }) {
     const checkActivePath = useActivePath()
     const isMobile = useMediaQuery("(max-width:640px)")
-    return <div className="sticky flex flex-col justify-between bg-mantle" style={{ width: isMobile ? '100%' : open ? "10rem" : "3rem", height: isMobile ? open ? "100dvh" : "56px" : "100vh", right: open ? 0 : undefined, top: 0, bottom: 0 }}>
+    return <div className="sticky flex flex-col justify-between bg-mantle nav" style={{ width: isMobile ? '100%' : open ? "10rem" : "3rem", height: isMobile ? open ? "100dvh" : "56px" : "100vh", right: open ? 0 : undefined, top: 0, bottom: 0, transition: 'width 0.3s ease-in-out' }}>
         <div>
             <div className="flex justify-between items-center w-100">
                 <Hamburger toggled={open} toggle={setOpen} size={16} hideOutline rounded />
                 {isMobile && <div className="flex p-4 items-center justify-center"><DarkModeSwitcher /></div>}
             </div>
-            <div className="px-2">
+            <div className="px-2 nav">
                 {/* <div className="flex gap-4 items-center" style={{ flexDirection: isMobile ? open ? "row" : "row" : "column", alignItems: isMobile ? 'center' : open ? "start" : "center" }}>
                     <FontAwesomeIcon icon={faBars} className="cursor-pointer m-1 " onClick={() => setOpen(!open)}></FontAwesomeIcon>
                     <DarkModeSwitcher />
@@ -51,7 +51,7 @@ export default function Sidebar({ open, setOpen }: { open: boolean, setOpen: Dis
                 {(!isMobile || open) && <div className="flex flex-col gap-4" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
                     {sidebarItems.map((item, i) => <Link key={i} href={item.href} onClick={() => isMobile ? setOpen(false) : undefined} className={`flex ${checkActivePath(item.href) ? "text-mauve" : 'text-text'} h-8 no-underline px-2 items-center gap-2  hover:bg-crust py-2 rounded cursor-pointer`}>
                         <FontAwesomeIcon icon={item.icon} className="text-sm text-inherit" ></FontAwesomeIcon>
-                        <p className="text-sm no-underline" style={{ opacity: open ? 1 : 0, position: open ? "sticky" : "absolute" }}> {item.label}</p>
+                        <p className="text-sm no-underline nav" style={{ opacity: open ? 1 : 0, position: open ? "sticky" : "absolute", transition: 'opacity 0.3s' }}> {item.label}</p>
                     </Link>)}
 
                 </div>}
