a55875e9de9646ac9aeb19e3883eb410ac4927fa
keerthimahadevan <keerthimahadevan@users.noreply.github.com>
2024-02-03 18:34:58 +0000

    deploy: coding-club-gct/homepage@12fad790eee820bdcec466b254f55e46ebdd9b90

--- apps/gallery/src/app/page.tsx
+++ apps/gallery/src/app/page.tsx
@@ -1,24 +1,24 @@
 import DarkModeSwitcher from "@/components/darkmodeswitcher";
 import logo from "@/assests/codingclublogo.png";
-import img from "@/assests/img.png";
+import cover from "@/assests/img.png";
 import { Button, Container, Divider, Link } from "@mui/material";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faDiscord, faInstagram, faGithub, faYoutube, faLinkedin, faWhatsapp, faTelegram, faInstagramSquare, faWhatsappSquare } from '@fortawesome/free-brands-svg-icons';
 import Footer from "@/components/footer"
 import Header from "@/components/header"
 import clg from "@/assests/college.webp"
-const Items = [
+const items = [
   {
-    title: 'Title 1', photo: img.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
+    title: 'Title 1', photo: cover.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
   },
   {
-    title: 'Title 2', photo: img.src, content:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
+    title: 'Title 2', photo: cover.src, content:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
   },
   {
-    title: 'Title 3', photo: img.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
+    title: 'Title 3', photo: cover.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
   },
   {
-    title: 'Title 3', photo: img.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
+    title: 'Title 3', photo: cover.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
   }
 
 ]
@@ -38,13 +38,12 @@ export default function Home() {
          Sapiente eaque sequi quasi libero iste explicabo, saepe quis. Rerum distinctio explicabo harum.</p>
       </div>
     </div>
-    {/* <div className="flex flex-col">
+    <div className="flex flex-col">
         <p className="text-4xl p-8 font-medium">RECENT EVENTS</p>
       <div className=" grid grid-rows-2 grid-cols-2 p-4 gap-4">
-        {
-          Items.map((item, i) => <div key={i} >
+        {items.map((item, i) => <div key={i} >
             <div className="relative flex flex-col gap-4 p-4 bg-mantle">
-              <img src={item.photo} alt="img" className="w-full  object-contain hover:opacity-40" />
+              {/* <img src={item.photo} alt="img" className="w-full  object-contain hover:opacity-40" /> */}
               <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0
         hover:opacity-100 transition-opacity duration-300">
                 <a href="www.google.com">
@@ -55,11 +54,9 @@ export default function Home() {
             <p className=" text-text font-semibold">{item.content}</p>
             </div>
 
-          </div>
-          )
-        }
+          </div>)}
       </div>
-      <div className="bg-mantle flex flex-col gap-2  mt-4">
+      {/* <div className="bg-mantle flex flex-col gap-2  mt-4">
         <p className="text-4xl font-medium mt-4 pl-8">THINGS TO BE NOTED</p>
         <div className=" grid grid-rows-1 grid-cols-3 gap-8 m-8">
           <div className=" bg-white flex flex-col p-4  h-50 gap-12 " >
@@ -88,8 +85,8 @@ export default function Home() {
             </div>
          
         </div>
-      </div>
-      <div className="flex flex-col p-4 gap-8">
+      </div> */}
+      {/* <div className="flex flex-col p-4 gap-8">
         <div className="flex justify-center">
         <p className="text-4xl font-medium p-4" >WHAT THEY SAY</p>
         </div>
@@ -148,8 +145,8 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
-      <Footer /> */}
-    {/* </div> */}
-  </div>
+      </div> */}
+      <Footer /> 
+    </div>
+  </div> 
 }
