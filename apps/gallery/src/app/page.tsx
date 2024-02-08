79258ad64e62f9d6a4c5fad4bf8cd406e438339e
useEffects <useEffects@users.noreply.github.com>
2024-02-08 19:31:40 +0000

    deploy: coding-club-gct/homepage@7a27dfeca0205c94c9cbcc02135f67a83bff87fb

--- apps/gallery/src/app/page.tsx
+++ apps/gallery/src/app/page.tsx
@@ -4,7 +4,7 @@ import cover from "@/assests/img.png";
 import { Avatar, AvatarGroup, Button, Container, Divider, MobileStepper } from "@mui/material";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDiscord, faInstagram, faYoutube, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faArrowLeft, faArrowRight, faBookmark, faCalendar, faComment, faEarth, faEnvelope, faHeart, faPeopleGroup, faSave, faTag, faUpload } from "@fortawesome/free-solid-svg-icons"
+import { faArrowLeft, faArrowRight, faBookmark, faCalendar, faComment, faEarth, faEnvelope, faHeart, faPeopleGroup, faSave, faTag, faUpload, faUser, faUsers } from "@fortawesome/free-solid-svg-icons"
 import Header from "@/components/header"
 import { team } from "@/lib/team"
 import { useContext, useEffect, useState } from "react";
@@ -17,21 +17,6 @@ import ccdark from "@/assests/codingclub/dark.png"
 import cclight from "@/assests/codingclub/light.png"
 import cartoon from "@/assests/cartoon.jpg"
 
-const items = [
-  {
-    title: 'Title 1', photo: cover.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
-  },
-  {
-    title: 'Title 2', photo: cover.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
-  },
-  {
-    title: 'Title 3', photo: cover.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
-  },
-  {
-    title: 'Title 3', photo: cover.src, content: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus qui repellat quidem libero minus illo fugit at molestiae natus provident deserunt nostrum ipsum esse voluptas, vero totam cumque consequatur quod tempore itaque, aut eveniet? Sed nemo et accusantium eos illum aliquam cupiditate quam esse minus provident beatae, quis ipsa blanditiis distinctio incidunt asperiores ad adipisci itaque corrupti, qui repudiandae iure."
-  }
-
-]
 const socialMediaItems = [
   { icon: faDiscord, href: "" },
   { icon: faInstagram, href: "" },
@@ -102,36 +87,42 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="p-4 md:gap-4 gap-2 ">
-        <p className="text-3xl p-4 "> Operation and HR team</p>
-        <Divider></Divider>
-        <div className="flex flex-col md:flex-row w-full">
-          <div className="md:w-1/2 md:p-20  p-4">
-            <div className="flex md:h-[600px] object-contain md:gap-6 ">
+      <div className="p-4 md:gap-4 gap-2 mt-40 mb-4">
+        <div className="my-4">
+          <p className="text-lg my-4"> WHAT WE DO </p>
+          <Divider></Divider>
+        </div>
+        <div className="flex flex-col md:flex-row w-full items-center gap-12">
+          <div className="md:w-1/2 md:p-20 p-4">
+            <div className="text-lg">
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci assumenda iure ad neque nobis repudiandae officia magnam in commodi, esse ab deleniti nulla ea voluptates sed nemo expedita quod necessitatibus voluptatem, sequi suscipit placeat fugit perferendis? Quas quisquam minima temporibus sapiente repellendus ducimus dolorem libero quidem, voluptatum dolor nesciunt veniam.</p>
-              <Divider orientation="vertical" className=" h-0  md:h-full "></Divider>
             </div>
           </div >
-          <div className="md:w-1/2 ">
-            {serviceItems.map((item, i) => <div className="flex ">
+          <Divider orientation="vertical" className="h-[800px]"></Divider>
+          <div className="md:w-1/2 flex flex-col gap-12 py-4">
+            {serviceItems.map((item, i) => <div className="flex">
               <div className="md:p-8 p-2">
                 <FontAwesomeIcon icon={item.icon} className="h-16 w-16 text-pink justify-center my-2" ></FontAwesomeIcon>
               </div>
               <div className="flex flex-col md:mt-8 mt-0 md:gap-4 ml-8">
                 <p className="text-2xl p-2 text-pink">{item.heading}</p>
                 <p className="text-subtext0  p-2">{item.content}</p>
-                <Divider className=""></Divider>
+                <Divider className="mt-12"></Divider>
               </div>
             </div>
             )}
           </div>
         </div>
       </div>
-      <div className="">
-        <div className="md:flex  gap-2 p-4 md:h-[850px] object-contain">
-          <div className="md:w-1/4  h-full flex flex-col gap-2">
-            <div className="w-full h-[300px] bg-crust rounded-xl justify-between md:p-4 p-2 flex flex-col">
-              <p className="text-3xl"> Operations and HR Team </p>
+      <div className="mt-40 mb-4">
+        <div className="my-4 px-4">
+          <p className="text-lg my-4"> ABOUT US </p>
+          <Divider />
+        </div>
+        <div className="md:flex gap-2 p-4 md:h-[850px] object-contain">
+          <div className="md:w-[30%] h-full flex flex-col gap-2">
+            <div className="w-full h-[300px] bg-crust rounded-xl justify-evenly md:py-4 md:px-8 p-2 flex flex-col">
+              <p className="text-3xl"> Activities Done Right </p>
               <p className="text-subtext0">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, explicabo!</p>
               <AvatarGroup>
                 {team.map((item, i) => <Avatar src={item.photo} key={i} />)}
@@ -140,7 +131,7 @@ export default function Home() {
                 <Button className="text-mauve"> Join Now </Button>
               </a>
             </div>
-            <div className="w-full h-[calc(100%-300px-1rem)] bg-mantle rounded-xl justify-evenly p-4 flex flex-col">
+            <div className="w-full h-[calc(100%-300px-1rem)] bg-mantle rounded-xl justify-evenly p-2 md:px-8 md:py-4 flex flex-col">
               <p className="text-3xl">Coding Club GCT</p>
               <p className="text-lg">Not a <span className="text-yellow"> Rocket</span> Science</p>
               <p className="text-subtext0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus officiis debitis saepe, voluptatem ea nemo consequatur nisi enim veritatis, corrupti maiores deleniti eaque voluptate fugiat, tempore tenetur culpa! Numquam, laudantium!</p>
@@ -149,63 +140,84 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="md:w-[45%] flex flex-col gap-2 h-full my-2">
-            <div className="w-full md:flex gap-2 h-[100px]">
-              <div className="md:w-2/3 rounded-xl bg-mantle flex items-center p-4 h-full">
+          <div className="md:w-[45%] flex flex-col gap-2 h-full">
+            <div className="hidden w-full md:flex gap-2 h-[100px]">
+              <div className="md:w-2/3 rounded-xl bg-mantle flex items-center h-full px-8 py-4">
                 <img className="w-full object-contain" src={darkMode ? "/arrow_dark.png" : "arrow_light.png"} alt="" />
               </div>
-              <div className="md:w-1/3 my-2 rounded-xl p-4 bg-crust h-full flex items-center">
-                <p className="text-lg text-center"> No Coding Required 😱😱😱 </p>
+              <div className="md:w-1/3 rounded-xl bg-crust h-full flex flex-col items-center justify-center">
+                <p className="text-lg text-center"> No Coding Required </p>
+                <p className="text-lg text-center"> 😱😱😱 </p>
               </div>
             </div>
-            <div className="w-full h-[calc(100%-100px-1rem)] rounded-xl">
-              <div className="flex  gap-2 h-[300px] items-center">
-                <div className="w-1/2  flex flex-col justify-between items-center h-[calc(100%-0.5rem)] mb-2 bg-mantle rounded-xl p-4">
-                  <img className="h-4/5 object-contain" src={darkMode ? "/dark.png" : "/light.png"} alt="" />
-                  <p className="text-subtext0 text-lg text-center"> Lorem ipsum dolor sit amet. </p>
+            <div className="w-full md:h-[calc(100%-100px-1rem)] rounded-xl">
+              <div className="flex gap-2 h-[300px] items-center">
+                <div className="w-1/2  flex flex-col justify-center items-center h-[calc(100%-0.5rem)] mb-2 bg-mantle rounded-xl px-8">
+                  <p className="text-3xl text-center"> Operations and HR Team </p>
+                  <div className="flex items-center">
+                    <img className="h-32 object-contain" src={darkMode ? "/dark.png" : "/light.png"} alt="" />
+                    <p className="text-subtext0 text-lg"> Lorem ipsum dolor sit. </p>
+                  </div>
                 </div>
-                <div className="w-1/2 bg-crust rounded-tl-xl rounded-tr-xl h-full flex flex-col p-4 justify-evenly">
-                  <img src={cartoon.src} alt="try later" className="w-full object-contain"></img>
-                  {/* <p className="text-3xl text-subtext0">Maintaining</p> */}
-                  <p className="text-4xl font-bold">{count}+<span className="text-xl font-normal">members</span></p>
+                <div className="w-1/2 bg-crust rounded-tl-xl rounded-tr-xl h-full flex flex-col py-4 px-8 justify-around">
+                  <div>
+                    <div className="flex justify-between items-center my-0">
+                      <p className="text-subtext0 text-2xl"> Members </p>
+                      <FontAwesomeIcon className="p-4 rounded-full bg-base w-4 h-4 text-pink" icon={faUsers} />
+                    </div>
+                    <div className="flex items-baseline">
+                      <p className="text-6xl"> {count} </p>
+                      <p className="text-4xl">+</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="flex justify-between items-center my-0">
+                      <p className="text-subtext0 text-2xl"> Events </p>
+                      <FontAwesomeIcon className="p-4 rounded-full text-pink w-4 h-4 bg-base" icon={faCalendar} />
+                    </div>
+                    <div className="flex items-baseline">
+                      <p className="text-6xl"> {events.length} </p>
+                      <p className="text-4xl">+</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="bg-crust rounded-xl rounded-tr-none h-[calc(100%-300px)] p-4">
+              <div className="bg-crust rounded-xl rounded-tr-none h-[calc(100%-300px)] p-2">
                 <img src="/team-work.jpg" className="w-full h-full rounded-xl" alt="" />
               </div>
             </div>
           </div>
-          <div className="w-[30%] flex flex-col h-full gap-2">
-            <div className="w-full h-[calc(100%-100px-1rem)] flex flex-col gap-4 bg-mantle p-4 rounded-xl">
-              <p className="text-3xl my-2"> Follow us on Instagram </p>
-              <div className="bg-base h-full w-full flex flex-col justify-evenly ">
-                <div className="flex gap-4 items-center p-2" >
+          <div className="w-[25%] flex flex-col h-full gap-2">
+            <div className="w-full h-[calc(100%-100px-1rem)] flex flex-col gap-4 bg-mantle p-2 rounded-xl">
+              <p className="text-3xl m-2"> Follow us on Instagram </p>
+              <div className="bg-base h-full w-full flex flex-col justify-evenly">
+                <div className="flex items-center py-2 px-4 gap-4">
                   <img src={darkMode ? ccdark.src : cclight.src} alt="try later" className="rounded-full w-8 h-8"></img>
-                  <div className="flex flex-col">
+                  <div>
                     <p>codingclub.gct</p>
                     <p className="text-subtext0 text-sm">Follow us</p>
                   </div>
-                  <a href="" className="text-blue"><Button className="text-sm " >View Profile</Button></a>
+                  <a className="mr-0 ml-auto" href=""><Button className="text-sm" variant="contained">View Profile</Button></a>
                 </div>
                 <div >
-                  <img src={cartoon.src} className="w-full object-contain"></img>
+                  <img src={cartoon.src} className="h-[350px] w-full"></img>
                 </div>
-                <a href=" " className="p-2 text-blue no-underline"><Button className="text-blue">View more on Instagram</Button></a>
+                <a href=" " className="text-blue no-underline px-2">View more on Instagram</a>
                 <Divider></Divider>
-                <div className="flex justify-between px-4" >
-                  <div className="flex  gap-4 " >
-                    <FontAwesomeIcon icon={faHeart} className="w-6 h-6"></FontAwesomeIcon>
-                    <FontAwesomeIcon icon={faComment} className="w-6 h-6"></FontAwesomeIcon>
-                    <FontAwesomeIcon icon={faUpload} className="w-6 h-6"></FontAwesomeIcon>
+                <div className="flex justify-between px-2">
+                  <div className="flex  gap-4">
+                    <FontAwesomeIcon icon={faHeart} className="w-4 h-4"></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faComment} className="w-4 h-4"></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faUpload} className="w-4 h-4"></FontAwesomeIcon>
                   </div>
                   <div className="flex ">
-                    <FontAwesomeIcon icon={faBookmark} className="w-6 h-6"></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faBookmark} className="w-4 h-4"></FontAwesomeIcon>
                   </div>
                 </div>
                 <Divider></Divider>
-                <div className="flex px-4 justify-between ">
+                <div className="flex px-2 justify-between ">
                   <p>add a comment</p>
-                  <a href=" "><FontAwesomeIcon icon={faInstagram} className="w-6 h-6"></FontAwesomeIcon></a>
+                  <a href=" "><FontAwesomeIcon icon={faInstagram} className="w-4 h-4"></FontAwesomeIcon></a>
                 </div>
               </div>
             </div>
@@ -218,10 +230,12 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div>
-        <div className="flex flex-col gap-4 mt-20 mb-4 p-4  ">
-          <p className="text-4xl">MEET OUR TEAM</p>
-          <p className="text-subtext0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, pariatur.</p>
+      <div className="mt-40 mb-4">
+        <div className="px-4">
+          <div className="my-4">
+            <p className="text-lg">MEET OUR TEAM</p>
+            <p className="text-subtext0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, pariatur.</p>
+          </div>
           <Divider></Divider>
         </div>
         <div className="md:flex-row flex flex-col  p-6  gap-4 h-[600px] ">
@@ -231,29 +245,30 @@ export default function Home() {
             <p className="md:text-5xl text-2xl font-bold text-pink"> {team[current].designation} </p>
           </div>
           <div className="md:w-1/2 h-full ">
-            <img src={team[current].photo} className="w-full object-contain h-full" alt="" />
+            <img src={team[current].photo} className="w-full bg-pink border-solid border-[20px] border-pink object-contain h-full" alt="" />
           </div>
         </div>
         <div className="flex justify-center">
           <MobileStepper className="bg-base text-text w-1/2" activeStep={current} variant="text" steps={team.length} position="static" nextButton={<FontAwesomeIcon icon={faArrowRight} className="cursor-pointer w-12 h-12 text-text" onClick={() => setCurrent(p => p === team.length - 1 ? 0 : p + 1)} />} backButton={<FontAwesomeIcon className="cursor-pointer w-12 h-12 text-text" icon={faArrowLeft} onClick={() => setCurrent(p => p === 0 ? team.length - 1 : p - 1)} />} />
         </div>
       </div>
-      <div className="md:w-full md:p-4 ">
-        <Divider></Divider>
-        <p className="text-4xl p-4">Recent Events</p>
-        <Divider></Divider>
-      </div>
-      <div className="grid grid-col-1 md:grid-cols-2 gap-8 m-4 ">
-        {events.map((item, i) => <div key={i} >
-          <div className="flex flex-col gap-8">
-            <img src={item.photo} className=" h-[50vw]"></img>
-            <div className="flex flex-col md:flex-row justify-around">
-              <p className="md:text-4xl text-2xl">{item.name}</p>
-              <Link href={item.slug}><Button>see more</Button></Link>
+      <div className="mt-40 mb-4">
+        <div className="my-4 px-4">
+          <p className="text-lg my-4"> EVENTS </p>
+          <Divider></Divider>
+        </div>
+        <div className="grid grid-col-1 md:grid-cols-2 gap-8 m-4">
+          {events.map((item, i) => <div key={i} >
+            <div className="flex flex-col gap-8">
+              <img src={item.photo} className=" h-[50vw]"></img>
+              <div className="flex flex-col md:flex justify-around">
+                <p className="md:text-4xl text-2xl">{item.name}</p>
+                <Link href={item.slug}><Button>see more</Button></Link>
+              </div>
             </div>
           </div>
+          )}
         </div>
-        )}
       </div>
     </div>
   </div>
